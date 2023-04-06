4eb53f57587aae3befde52ba7e084fc9b362a88a
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2023-04-06 09:14:45 +0000

    新規登録、ログイン、ゲストログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'public/homes#top'
+
+  # 会員用
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  devise_scope :user do
+    post "/users/sign_out", to: "devise/sessions#destroy"
+    post "/users/guest_sign_in", to: "public/sessions#guest_sign_in"
+  end
+
+  # 管理者用
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 end
