e241588da811eafffadfc154ebbe275db3863709
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2023-04-15 00:41:12 +0000

    [Add]レシピ投稿のバリデーション

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,11 @@
 class Recipe < ApplicationRecord
   has_one_attached :recipe_image
 
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :serving, presence: true
+  validates :recipe_image, attached_file_presence: true
+
   belongs_to :user
   has_many :ingredients, dependent: :destroy
   accepts_nested_attributes_for :ingredients, allow_destroy: true
