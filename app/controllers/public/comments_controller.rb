121c414df86d10db35c23b77c6540e8aaba052e9
marimo0283 <kugelschreiber1002@gmail.com>
2023-04-16 13:10:02 +0000

    [Update]いいね・コメントの非同期通信化

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,15 +1,21 @@
 class Public::CommentsController < ApplicationController
+
   def create
-    recipe = Recipe.find(params[:recipe_id])
+    @recipe = Recipe.find(params[:recipe_id])
     comment = current_user.comments.new(comment_params)
-    comment.recipe_id = recipe.id
+    comment.recipe_id = @recipe.id
     comment.save
-    redirect_to recipe_path(recipe)
+    @comment = Comment.new
+    @ingredients = @recipe.ingredients
+    @steps = @recipe.steps
+    
+    render "public/recipes/show"
   end
 
   def destroy
+    @recipe = Recipe.find(params[:recipe_id])
     Comment.find(params[:id]).destroy
-    redirect_to recipe_path(params[:recipe_id])
+    @comment = Comment.new
   end
 
   private
